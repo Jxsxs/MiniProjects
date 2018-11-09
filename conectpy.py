b4dee812b69d68a0b6ee8a1a90a8546c5d2bb8de
Jesus Montalvo <chuy_ronald@hotmail.com>
2018-11-09 12:02:49 -0600

    Se jalan los logs de la 20 a la 193

--- conectpy.py
+++ conectpy.py
@@ -4,14 +4,13 @@ import psycopg2.extras
 import pprint
 import time
 
-
 # se crea la conexion hacia la base de datos de sqlServer
-# server = '172.16.126.20,1433\DBSERVER'
-# database = 'MCASDB'
-# username = 'sa'
-# password = '@dmin123'
-# cnxn = pyodbc.connect('DRIVER={ODBC Driver 11 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
-# cursorSql = cnxn.cursor()
+server = 'DBSERVER'
+database = 'MCASDB'
+username = 'sa'
+password = '@dmin123'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 11 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
+cursorSql = cnxn.cursor()
 # ===============================================================
 # Aca se hace la conexion a postgres
 conn = psycopg2.connect(database='tlcmvdb' , user='postgres', password='star2018',host='172.16.126.193')
@@ -19,24 +18,22 @@ cursorPost = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 # ================================================================
 ultimoId = "0"
 while True:
+    cursorPost.execute("select logid from logsmscommands order by logid desc limit 1")
 
-    cursorPost.execute("select serial_number from packages where serial_number::bigint>"+ ultimoId  +"::bigint order by serial_number asc limit 5")
-    #Consultar la Base de datos desde Sql
-    # cursorSql.execute("select TOP (5) * FROM LogSMSCommands;") #logSMSCommands
-    # ultimoDato
     #leer Registros desde sql
-    row = cursorPost.fetchall()
-    # row = cursorSql.fetchall()
-    for col in row:
-        print(col)
-        ultimoId = col[0]
-        # cursorPost.execute("insert into logsmscommands(log_id, log_date, log_client_ip, sms_operator_tag, sc_serial_number, cmd_id, log_result, plain_command, actual_serial_number) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)", col)
-    # conn.commit()
-    # cursorSql.close()
-    # cursorPost.close()
-    # conn.close()
-    # cnxn.close()
+    rowPost = cursorPost.fetchall()
+
+    for colPost in rowPost:
+        ultimoId = colPost[0]
+
+    #Consultar la Base de datos desde Sql
+    cursorSql.execute("select top 1000 * FROM LogSMSCommands where logId > " + str(ultimoId) + " order by logId asc") #logSMSCommands
+    rowSql = cursorSql.fetchall()
+    for colSql in rowSql:
+        print(colSql[0])
+        cursorPost.execute("insert into logsmscommands(logid, logdate, logclientip,logclientport ,smsoperatortag, scserialnumber, cmdid, logresult, plaincommand, actualserialnumber) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", colSql)
+
     #imprimir registros
-    # pprint.pprint(row)
-    # print("ultimo: " + ultimoId)
-    time.sleep(10)
+    conn.commit()
+    print("ultimo: " + str(ultimoId))
+    time.sleep(60)
